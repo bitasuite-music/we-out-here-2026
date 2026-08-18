8b893c61b821296343f6f584dbb3aef454e0a152
bitasuite-music <phil@phcapability.co.uk>
2026-08-18 11:22:58 +0100

    Version increment to force cache refresh

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
  * timeout, so the cached copy always wins. When there IS signal the fetch
  * still runs and refreshes the cache for next time.
  */
-var VER = '8c26a7f5b1';  // built 2026-08-18
+var VER = '8c26a7f5b2';  // built 2026-08-18
 var CACHE = 'woh26-' + VER;
 
 var ASSETS = [
